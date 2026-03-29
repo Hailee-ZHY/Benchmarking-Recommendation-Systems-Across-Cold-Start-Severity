f2c7dbdadcb267276ff55ad8f9f46d5700a59741
Hailee-ZHY <hanyi_zhang_2@sfu.ca>
2026-03-29 10:43:05 -0700

    als data, training, testing

--- ALS.py
+++ ALS.py
@@ -6,4 +6,53 @@ from pyspark.ml.feature import StringIndexer
 from pyspark.ml import Pipeline
 from pyspark.sql.functions import col
 
+from DataProcesser import DataProcessing
 
+processer = DataProcessing()
+
+df_train = processer.train_als
+df_test = processer.cold_start_data  # can be changed back to test_als
+
+def train_asl(
+    df_train, 
+    user_col="user_id", 
+    item_col="parent_asin", 
+    rating_col="rating", 
+    max_iter=5, 
+    reg_param=0.09, 
+    rank=25, 
+    cold_start_strategy="drop"
+):
+    # index columns
+    user_indexer = StringIndexer(inputCol=user_col, outputCol="user_index", handleInvalid="skip")
+    item_indexer = StringIndexer(inputCol=item_col, outputCol="item_index", handleInvalid="skip")
+
+    pipeline = Pipeline(stages=[user_indexer, item_indexer])
+    df_train_indexed = pipeline.fit(df_train).transform(df_train)
+    
+    # train als 
+    als = ALS(
+        maxIter=max_iter, 
+        regParam=reg_param, 
+        rank=rank,
+        userCol="user_index", 
+        itemCol="item_index", 
+        ratingCol=rating_col, 
+        coldStartStrategy=cold_start_strategy,
+    )
+
+    als_model = als.fit(df_train_indexed)
+    return als_model, pipeline
+
+def test_asl(als_model, pipeline, df_test, user_col, item_col):
+    # index test data
+    df_text_indexed = pipeline.transform(df_test)
+    predictions = als_model.transform(df_text_indexed)
+
+    return predictions
+
+if __name__ == "__main__":
+    als_model, pipeline = train_asl(df_train)
+    predictions = test_asl(als_model, pipeline, df_test, "user_id", "parent_asin")
+
+    predictions.show(10)
--- DataProcesser.py
+++ DataProcesser.py
@@ -1,4 +1,7 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.functions import col, trim, isnan
+
 import os
 
 # 1. spark for review dataset
@@ -9,6 +12,7 @@ spark = SparkSession.builder\
     .config('spark.sql.shuffle.partitions', '20')\
     .config('spark.memory.fraction', '0.6')\
     .config('spark.driver.maxResultSize', '1g')\
+    .config("spark.dynamicAllocation.enabled", "true")\
     .getOrCreate()
 
 # 2. Preprocessing
@@ -18,18 +22,56 @@ class DataProcessing:
               self.df_review = spark.read.parquet('my_amazon_books.parquet')
               # Meta Data
               self.df_meta = spark.read.parquet('my_amazon_books_meta.parquet')
+              self.train_als, self.test_als, self.meta_als = self.Data_Process(method = 'ALS')
+              self.cold_start_data = self.simulate()
+              
 
         def Data_Process(self, method = 'ALS'):
                if method == 'ALS':
                       # only need: user_id, item_id, rating
-                      data_review_als = self.df_review.select('user_id', 'parent_asin', 'rating')
-                      data_meta_als = self.df_meta.select('title', 'parent_asin')
-               return data_review_als, data_meta_als
+                      df_review_clean = self.df_review.filter((col("user_id").isNotNull()) 
+                                                              & (col("parent_asin").isNotNull())
+                                                              & (trim(col("user_id")) != "")
+                                                              & (trim(col("parent_asin")) != ""))
+
+                      data_review_als = df_review_clean.select('user_id', 'parent_asin', 'rating') # this is used for training                     
+                      data_meta_als = self.df_meta.select('title', 'parent_asin') # this is use for provide book title in the recommendation system
+                      # split data into training and testing set
+                      train_df, test_df = data_review_als.randomSplit(weights = [0.7, 0.3], seed = 42)
+               return train_df, test_df, data_meta_als
 
 ### Simulate the sparse dataset (cold start)
 # TODO: k can be the arg passed to the function
 
+        def simulate(self, alpha = 100, k = 1):
+               # methods means the filter methods, we can choose ["both", "user", "item"]
+               # k means the ratio of cold start
 
+               # Step1. select users / items could be used for simulation
+
+                user_rating_count = self.test_als.groupBy("user_id").agg(
+                       F.count("rating").alias("rating_cnt_by_user")
+                        )
+                filtered_user_id = user_rating_count.filter(F.col("rating_cnt_by_user") >= alpha).select("user_id")
+
+                item_rating_count = self.test_als.groupBy("parent_asin").agg(
+                      F.count("rating").alias("rating_cnt_by_item")
+                        )
+
+                filtered_item_id = item_rating_count.filter(F.col("rating_cnt_by_item") >= alpha).select("parent_asin")
+                     
+                cold_simulate_df = self.test_als.join(filtered_user_id, on = "user_id", how = "inner") \
+                                                .join(filtered_item_id, on = "parent_asin", how = "inner")
+                """
+                test_data: [user_id, parent_asin, rating]
+                cold_simulate:[user_id, parent_asin, rating]
+                """
+
+               # step2. using K to remove rating record for users (temperarily only for users)
+                # Todo
+                return cold_simulate_df
+                
+      
 # 2. Overview of dataset
 def data_overview(df):
         print(f'review data summary: {df.count()}')
@@ -46,5 +88,50 @@ if __name__ == "__main__":
         # Review Dataset
         # data_overview(df_review)
         # Meta Dataset
-        data_overview(dp.df_meta)
-        dp.df_meta.show()
+        # train, test, meta = dp.Data_Process()
+        # print(f"train count: {dp.train_als.count()}")
+        # print(f"test count: {dp.test_als.count()}")
+        # print(f"meta count: {dp.meta_als.count()}")
+        # user, item = dp.simulate()
+        # print("user simulate data cols:")
+        # for col, dtype in user.dtypes:
+        #        print(f"{col}:{dtype}")
+
+        # print("item simulate data cols:")
+        # for col, dtype in item.dtypes:
+        #        print(f"{col}:{dtype}")
+
+        ## check again: does user_id has null value? -- confirm: there is no null value in user_id 
+        # user_null_cnt = user.filter(col("user_id").isNull()).count()
+        # print(user_null_cnt)
+
+        # cold_df = dp.simulate()
+        # cold_df.describe().show()
+
+"""
+Notes:
+Data Summary for ALS: 
+train count: 20,628,076                                                           
+test count: 8,847,377                                                             
+meta count: 4,448,181
+
+Count the number of ratings for users and items:
+
+
+1. user_id has non-value -- remove null value  -- still have null value
+2. item shid has non-value -- remove null value
+
+
+In test data (30%), we need to filter out users satifying the criteria. 
+actually, the # of user parcitipainting into the test is much lower than 20% of the data set
+Only 9639 rows. 
++-------+--------------------+--------------------+------------------+          
+|summary|         parent_asin|             user_id|            rating|
++-------+--------------------+--------------------+------------------+
+|  count|                9639|                9639|              9639|
+|   mean|                    |                NULL| 4.201888162672477|
+| stddev|                    |                NULL|0.9609044575690069|
+|    min|                    |                    |               1.0|
+|    max|                    |                    |               5.0|
++-------+--------------------+--------------------+------------------+
+"""
